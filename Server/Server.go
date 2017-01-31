73022daddf0f9780bc215a5f328f9f6d62aff345
ShadowJonathan <jonathandejong02@gmail.com>
2017-01-31 20:04:46 +0100

    added some more stuff

--- Server/Server.go
+++ Server/Server.go
@@ -15,11 +15,11 @@ func main() {
 		fmt.Println("Error Loading Server: " + y)
 	}
 	ZF := http.NewServeMux()
-	ZF.HandleFunc("/", ZFic.MainPage())
-	ZF.HandleFunc("/a", ZFic.Archive)
-	ZF.HandleFunc("/f", ZFic.Fic)
+	ZF.HandleFunc("/", ZFic.MainPage()) // the front page
+	ZF.HandleFunc("/a", ZFic.Archive)   // archive handler, aka search
+	ZF.HandleFunc("/f", ZFic.Fic)       // story handler, aka fanfiction
 
-	ZF.HandleFunc("/static", ZFic.Static())
+	ZF.HandleFunc("/static", ZFic.Static()) // the css and other shit >.> (probably images and stuff)
 
 	log.Fatal(http.ListenAndServe(":8080", ZF))
 }
